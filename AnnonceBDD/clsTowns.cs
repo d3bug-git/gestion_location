b06343c49c01af9cd66f56b482898ba483694f6a
serge watchou <swatchou@yahoo.com>
2020-03-18 18:28:24 +0100

    lien entre classes sandwich

--- AnnonceBDD/clsTowns.cs
+++ AnnonceBDD/clsTowns.cs
@@ -12,12 +12,6 @@ namespace AnnonceBDD
         public string NameTown { get; set; }
         public string PostalCode { get; set; }
 
-        public Countries Country;
-        public int CountryID;
-
-        public ObservableCollection<Customers> Costumers { get; set; } = new ObservableCollection<Customers>();
-        public ObservableCollection<Owners> Owners { get; set; } = new ObservableCollection<Owners>();
-
         public event PropertyChangedEventHandler PropertyChanged;
     }
 }

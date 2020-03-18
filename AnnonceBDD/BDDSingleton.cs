0ec5a608ba9a5352dd1d952b84a17ec6dd2fae96
serge watchou <swatchou@yahoo.com>
2020-03-18 16:11:29 +0100

    Création de la BDD

---
+++ AnnonceBDD/BDDSingleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AnnonceBDD
+{
+    public class BDDSingleton
+    {
+
+        #region Propriétés représentant la Base de données ou la rendant accessible à l'extérieur du projet
+        public static BDDSingleton Instance { get; set; } = new BDDSingleton();
+        private BDDAnnonce BDD { get; set; }
+        #endregion
+
+        #region Propriétés
+        public bool ModificationsEnAttente => BDD?.ChangeTracker.HasChanges() ?? false;
+        #endregion
+
+        #region Constructeur de la classe
+        public BDDSingleton()
+        {
+            BDD = new BDDAnnonce();
+            BDD.Database.EnsureCreated();
+        }
+        #endregion
+
+        #region Méthodes effectuant des modifications/actions plus spécifiques sur les données
+        public void SauvegarderModifications() { BDD?.SaveChanges(); }
+        #endregion
+    }
+}

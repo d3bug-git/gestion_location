d084e0982019041c2754eb6faf74b14d4815f555
serge watchou <swatchou@yahoo.com>
2020-04-06 20:31:44 +0200

    AnnonceWpf est ok avec netCore get started

--- AnnonceWPF/MainWindow.xaml.cs
+++ AnnonceWPF/MainWindow.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using AnnonceBDD;
-using MahApps.Metro.Controls;
+using MahApps.Metro.IconPacks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,22 +15,22 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-
 namespace AnnonceWPF
 {
     /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : MetroWindow
+    public partial class MainWindow : Window
     {
-        //private BDDSingleton BDD = BDDSingleton.Instance;
+        private BDDSingleton BDD = BDDSingleton.Instance;
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+
             //Affichage d'un cadre par défaut.
             //Cadre.NavigationService.Navigate(new pgAdverts());
         }
-
         #region Méthodes liées au Menu du haut de la fenêtre
         //private void AfficherAnnonces(object sender, RoutedEventArgs e) { Cadre.NavigationService.Navigate(new pgAdverts()); }
         private void SauvegarderModifications(object sender, RoutedEventArgs e)
@@ -39,12 +39,28 @@ namespace AnnonceWPF
             MessageBox.Show("Modifications sauvegardées dans la base de données.", "Sauvegarde des modifications", MessageBoxButton.OK, MessageBoxImage.Information);
         }
         private void Quitter(object sender, RoutedEventArgs e) { this.Close(); }
+        private void BasculerState(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState.Equals(WindowState.Maximized))
+            {     
+                this.WindowState = WindowState.Normal;
+                this.WindowStateIcon.Kind=PackIconModernKind.WindowMaximize;
+
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+                this.WindowStateIcon.Kind = PackIconModernKind.WindowMinimize;
+            }
+        }
+        private void Normal(object sender, RoutedEventArgs e) { this.WindowState = WindowState.Normal; }
+        private void Mininiser(object sender, RoutedEventArgs e) { this.WindowState = WindowState.Minimized; }
         #endregion
 
-        private void NavServiceOnNavigated(object sender,NavigationEventArgs e) { while (Cadre.NavigationService.RemoveBackEntry() != null) ; }
+        private void NavServiceOnNavigated(object sender, NavigationEventArgs e) { while (Cadre.NavigationService.RemoveBackEntry() != null) ; }
         private void OnClosing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            /*if (BDD.ModificationsEnAttente)
+            if (BDD.ModificationsEnAttente)
             {
                 if (MessageBox.Show("Il y a des modifications en attente voulez vous les sauvegarder avant de quitter ?", "Application GestionPAE", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.No)
                 {
@@ -53,7 +69,8 @@ namespace AnnonceWPF
                         e.Cancel = true;
                     }
                 }
-                else { BDD.SauvegarderModifications(); }*/
-            }
+                else { BDD.SauvegarderModifications(); }
         }
+    }
+}
 }

3d24ba7622ae9407877a5dd94b416cd6ee4030b2
Armand BUYSE <armand.buyse@condorcet.be>
2020-04-14 19:43:08 +0200

    Design de Customer

---
+++ AnnonceWPF/pgCustomers.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using AnnonceBDD;
+
+namespace AnnonceWPF
+{
+    /// <summary>
+    /// Logique d'interaction pour pgCustomers.xaml
+    /// </summary>
+    public partial class pgCustomers : Page
+    {
+        BDDSingleton BDD = BDDSingleton.Instance;
+        public ReadOnlyObservableCollection<Customer> Customers => BDD.customers;
+        public ReadOnlyObservableCollection<Town> Towns => BDD.towns;
+        public pgCustomers()
+        {
+            InitializeComponent();
+            lvCustomers.DataContext = BDD.customers;
+        }
+        private void RemoveCustomer(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void AddCustomer(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void lvCustomers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lvCustomers.SelectedItem != null) { grpInfoCustomer.DataContext = (Customer)lvCustomers.SelectedItem; }
+        }
+
+        private void lvCustomers_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AnnonceWPF/pgOwners.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AnnonceBDD;
+
+namespace AnnonceWPF
+{
+    /// <summary>
+    /// Logique d'interaction pour pgOwners.xaml
+    /// </summary>
+    public partial class pgOwners : Page
+    {
+       BDDSingleton BDD = BDDSingleton.Instance;
+
+            public pgOwners()
+            {
+                InitializeComponent();
+                grpOwners.DataContext = BDD.owners;
+            }
+
+            private void AddOwner(object sender, RoutedEventArgs e)
+            {
+            }
+
+            private void RemoveOwner(object sender, RoutedEventArgs e)
+            {                
+            }
+        
+    }
+}
